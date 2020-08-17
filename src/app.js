b0f93911283bd2d3fc1ad4100318feb0b88eab6c
nostalgia <skfk0135@gmail.com>
2020-08-17 15:30:17 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(function(req, res) {
+    res.status(404).json({ message: "Not Found Error" });
+});
+
+app.use(function(err, req, res, next) {
+    res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
